84c4957e53de127d2ae682380bc5b7c844d93e84
Louis Grant <ljsgrant@gmail.com>
2022-01-11 11:06:30 +0000

    Working on Light Switch Controller

---
+++ Assets/Scripts/BasicLightSwitchController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicLightSwitchController : Interactable
+{
+
+    public GameObject lightToTurnOn;
+    public GameObject lightToTurnOn2;
+    public GameObject lightToTurnOn3;
+    public GameObject lightSwitch = null;
+    public bool isLightToTurnOnActive;
+
+    InteractRaycast interactRaycast;
+
+#region Light Sequence
+    // Comment this out for using with normal lights (whilst testing).
+    // Eventually need to build in functionality whereby Interact checks
+    // a tag on the light to see if it's part of a sequence or a discrete
+    // light?
+    private IEnumerator TurnOnLightSequenceCoroutine()
+    {
+        if (!isLightToTurnOnActive)
+        {            
+            lightToTurnOn.GetComponent<Light>().enabled = true;
+            isLightToTurnOnActive = true;
+            yield return new WaitForSeconds(2);
+            lightToTurnOn2.GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(1);
+            lightToTurnOn3.GetComponent<Light>().enabled = true;
+        }
+        else
+        {
+            lightToTurnOn.GetComponent<Light>().enabled = false;
+            isLightToTurnOnActive = false;
+            // Debug.Log("Turned light off");
+        }
+    }
+
+    public override void Interact()
+    {
+        if(interactRaycast.hit.collider.gameObject.layer == LayerMask.NameToLayer("LightInSequence"))
+        {
+            StartCoroutine(TurnOnLightSequenceCoroutine());
+            Debug.Log("light sequence");
+        }
+        if(interactRaycast.hit.collider.gameObject.layer == LayerMask.NameToLayer("LightStandalone"))
+        {
+            if (!isLightToTurnOnActive)
+            {            
+                lightToTurnOn.GetComponent<Light>().enabled = true;
+                isLightToTurnOnActive = true;
+            }
+            else
+            {
+                lightToTurnOn.GetComponent<Light>().enabled = false;
+                isLightToTurnOnActive = false;
+            }
+        }
+        else
+        {
+            Debug.Log("Couldn't find a light to work with!");
+        }
+
+    }
+    #endregion
+
+    // Comment this out for testing the light sequence above
+    #region Normal light Interact 
+    /*
+    public override void Interact()
+    {
+
+
+    }
+    */
+    #endregion
+
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
